c19d9f785f6724ca700ea3cc91706126f09e16cc
unknown <srisai7247@gmail.com>
2021-02-05 12:45:19 +0530

    once

--- src/components/onceFunc.jsx
+++ src/components/onceFunc.jsx
@@ -1,19 +1,25 @@
 
    
-const once = function (func) {
-    var resposne;
-    return function () {
-        if (func) {
-            resposne = func.apply(this, arguments);
-            func = null;
-        }
-        return resposne;
+function add(a,b) {
+    return a+b
+    
     }
-};
-function add(a, b) {
-    return a + b;
- }
-const onceAdd = once(add);
-
-alert(onceAdd(2, 1));
-alert(onceAdd(3, 4));
+    
+    
+    function once(func) {
+    let res;
+    return function (a,b) {
+    
+    if(res){
+        return res
+    }
+      else {
+        res = func.call(this, a, b)
+        return res
+    }
+    }
+    }
+    let onceadd = once(add) 
+    
+    alert(onceadd(1,2))
+    alert(onceadd(3,5))
